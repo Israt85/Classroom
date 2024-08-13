762096814f69a6841c783a912cd8a02446272d44
Israt85 <isratanjuman3@gmail.com>
2024-08-13 23:26:29 +0600

    added

--- src/Components/Dashboard/AllStudents/AllStudents.jsx
+++ src/Components/Dashboard/AllStudents/AllStudents.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { RiDeleteBin5Fill } from "react-icons/ri";
+import Swal from 'sweetalert2';
 
 const AllStudents = () => {
     const [students, setStudents] = useState([]);
@@ -9,14 +11,48 @@ const AllStudents = () => {
             .then(res => {
                 console.log(res.data);
                 // Filter out any users with the role of 'Admin'
-                const nonAdminStudents = res.data.filter(stu => stu.role == '');
+                const nonAdminStudents = res.data.filter(stu => stu.role == 'student');
                 setStudents(nonAdminStudents);
             })
             .catch(err => {
                 console.log(err);
             });
     }, []);
-    
+    const handleDelete=(user)=>{
+         console.log('clicked');
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+
+                axios.delete(`http://localhost:5000/user/${user._id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            })
+                            
+                        }
+                    })
+                    
+
+
+            }
+        });
+
+
+    }
+
     return (
         <div className="overflow-x-auto">
             <table className="table-auto w-full">
@@ -25,6 +61,7 @@ const AllStudents = () => {
                         <th className="px-4 py-2">#</th>
                         <th className="px-4 py-2">Name</th>
                         <th className="px-4 py-2">Email</th>
+                        <th className="px-4 py-2">Role</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -34,6 +71,9 @@ const AllStudents = () => {
                                 <td className="border px-4 py-2 text-center">{index + 1}</td>
                                 <td className="border px-4 py-2 text-center">{stu?.name}</td>
                                 <td className="border px-4 py-2 text-center">{stu?.email}</td>
+                                <td className="border px-4 py-2 text-center">{stu?.role} </td>
+                                <button onClick={()=>handleDelete(stu)} className='text-xl my-2'><RiDeleteBin5Fill /> </button>
+                              
                             </tr>
                         ))
                     }
--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -22,7 +22,8 @@ const Registration = () => {
             .then(()=>{
                 const userInfo ={
                    name: data.name,
-                   email : data.email
+                   email : data.email,
+                   role: data.role,
                 }
                 
                 axios.post('http://localhost:5000/user', userInfo)
@@ -52,12 +53,13 @@ const Registration = () => {
     return (
         <div className='h-screen bg-base-100'>
         <div>
-        <h2 className='text-center my-6 text-green-700 font-extrabold text-2xl underline'>Register here.....</h2>
-        <div className='flex my-10 justify-evenly items-center'>
-            <div className='w-80 px-4 rounded-md py-10 bg-slate-300 h-[500px]' >
+       
+        <div className='flex my-5 justify-evenly items-center'>
+            <div className='w-80 px-4 rounded-md py-5 bg-slate-100 border border-purple-900 h-[600px]' >
 
 
                 <form onSubmit={handleSubmit(onSubmit)} className=" mx-auto">
+                <h2 className='text-center my-6 text-2xl'>Register here</h2>
                     <div className="mb-5">
                         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Photo url</label>
                         <input type="url" {...register("url")}className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required />
@@ -70,12 +72,22 @@ const Registration = () => {
                         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                         <input type="email" {...register("email")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                     </div>
+                    <div className='flex gap-4'>
                     <div className="mb-5">
                         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
                         <input type="password" {...register("password")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                    </div><div className="mb-5">
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
+                        <select className='p-2' {...register("role")} name="" id="">
+                        <option value="Select a role">select a role</option>
+                        <option value="Teacher">Teacher</option>
+                        <option value="Student">Student</option>
+
+                        </select>
+                    </div>
                     </div>
                     <div>
-                        <button type="submit" className="text-white w-full bg-green-700 hover:bg-green-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        <button type="submit" className="text-white w-full bg-purple-900 hover:bg-purple-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
                     </div>
                     {/* <SocialLogin></SocialLogin> */}
                     <h2 className='mt-4'>Already have an acoount? please <Link className='text-blue-800 font-bold hover:underline' to='/login'>Login</Link> </h2>
